c11279f0a47a6aa4236fa336d64436b041ed7074
TruenoxDDH <IIXdiazXII@gmail.com>
2022-09-29 12:27:02 -0500

    second

--- js/app.js
+++ js/app.js
@@ -2,25 +2,16 @@
 // variables
 
 const carrito = document.querySelector('#carrito');
-
-const contenedorCarrito = document,querySelector('#lista-carrito tbody');
-
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const listaCursos = document.querySelector('#lista-Cursos');
-
 const vaciarCarritoBtn =document.querySelector('#vaciar-carrito');
-
+let articulosCarrito = [];
 
 cargaEventListener();
 function cargaEventListener(){
 
 
-listaCursos.addEventListener(
-
-
-
-'click',agregarCurso);
-
-
+listaCursos.addEventListener('click', agregarCurso);
 
 }
 
@@ -38,5 +29,27 @@ console.log(e.target);
 
 }
 
+articulosCarrito =[...articulosCarrito,infoCurso];
 
+console.log(articulosCarrito);
+carritoHTML();
 }
+function carritoHTML(){
+
+limpiarHTML();
+
+    articulosCarrito.forEach(curso =>{
+const row = document.createElement('tr');
+row.inertHTML = `
+<td>
+  ${curso.titulo}
+</td>
+`;
+
+contenedorCarrito.appendChild(row);
+    });
+}
+function limpiarHTML(){
+    contenedorCarrito.innerHTML='';
+}
+//porqueeee
